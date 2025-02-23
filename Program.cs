ae894d6501397103a8101aaf0bef70721442acae
bobboo101 <48006021+bobboo101@users.noreply.github.com>
2025-02-24 09:27:55 +1030

    namespaces

--- Program.cs
+++ Program.cs
@@ -7,12 +7,35 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public struct Packet
+    {
+        public int packetID
+        {
+            get; private set;
+        }
+        public Packet(int _packetID) 
+        {
+            packetID = _packetID;
+        }
+    }
     namespace Server
     {
+        public class Server
+        {
+            public Server()
+            {
 
+            }
+        }
     }
     namespace Client
     {
+        public class Client
+        {
+            public Client()
+            {
 
+            }
+        }
     }
 }
