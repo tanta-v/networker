6c8e940e0618b2c2cd21a70a05fd0e14b663606c
bobboo101 <48006021+bobboo101@users.noreply.github.com>
2025-02-24 10:49:26 +1030

    update

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
-﻿namespace networker
+﻿using networker;
+using networker.Utility;
+using networker.Packetry;
+using networker.Client;
+using networker.Server;
+namespace networker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{Utility.Utility.timeNowAsString}: Hello, World!");
         }
     }
-    public struct Packet
+    namespace Utility
     {
-        public int packetID
+        public static class Utility
         {
-            get; private set;
+            public static long UTCTimeAsLong { get { return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(); } }
+            public static string timeNowAsString { get { return DateTimeOffset.Now.ToString(); } }
         }
-        public Packet(int _packetID) 
+    }
+    namespace Packetry
+    {
+        
+        public interface IPacket
         {
-            packetID = _packetID;
+            /// <summary>
+            /// The parent 'packet' interface. 
+            /// Basic inheritance structure is Packet>PacketType(data)
+            /// </summary>
+            public int packetID { get; set; } // the ID of the packet. Used to identify packets in the log, keeps synchronicity across client-server. Replicated.
+            public long timeSent { get; internal set; } //the UTC time that the packet was sent, defined as a 'long'. keeps track of ping. Replicated.
         }
+        public class PacketMaster
+        {
+            public 
+        }
+
     }
     namespace Server
     {
